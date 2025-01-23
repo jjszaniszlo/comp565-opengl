eb0670e627af8ba12f4e03ef70a99e5d899f0bd7
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-01-23 07:38:28 -0800

    use Catch2 instead of GoogleTest.  Also an initial example in test.cpp

---
+++ cmake/AddCatch2.cmake
@@ -0,0 +1,12 @@
+Include(FetchContent)
+
+FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v3.4.0 # or a later release
+)
+
+FetchContent_GetProperties(Catch2)
+if (NOT Catch2_POPULATED)
+    FetchContent_MakeAvailable(Catch2)
+endif()
