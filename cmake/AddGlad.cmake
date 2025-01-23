18cfb418f5555264eb5cda5dabb155a284b269dc
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-01-23 06:58:18 -0800

    reorganize

--- cmake/AddGlad.cmake
+++ cmake/AddGlad.cmake
@@ -10,7 +10,7 @@ if(NOT glad_POPULATED)
     FetchContent_Populate(glad)
 
     set(GLAD_PROFILE "core" CACHE STRING "OpenGL profile")
-    set(GLAD_API "gl=" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
+    set(GLAD_API "gl=3.3" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
     set(GLAD_GENERATOR "c" CACHE STRING "Language to generate the binding for")
     add_subdirectory(${glad_SOURCE_DIR} ${glad_BINARY_DIR})
 endif()
