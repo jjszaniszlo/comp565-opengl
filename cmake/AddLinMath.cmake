f4651f6f8fcf561970b70fc67f2b48a0e17407a1
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-01-23 06:26:04 -0800

    init

---
+++ cmake/AddLinMath.cmake
@@ -0,0 +1,11 @@
+FetchContent_Declare(
+        linmath
+        GIT_REPOSITORY https://github.com/datenwolf/linmath.h.git
+)
+
+FetchContent_GetProperties(linmath)
+if(NOT linmath_POPULATED)
+    FetchContent_Populate(linmath)
+    add_library(linmath INTERFACE)
+    target_include_directories(linmath INTERFACE ${linmath_SOURCE_DIR})
+endif()
